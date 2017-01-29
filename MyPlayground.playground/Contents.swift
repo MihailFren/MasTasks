b54205dd79eeb8f122c75d407f3b727e27af8f38
mihail fren <mihailfren@gmail.com>
2017-01-29 15:55:21 +0300

    задание 1

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,11 @@
 //: Playground - noun: a place where people can play
-
+var mas1 = ["a","b","c","a","b"]
+var mas2 = [String]()
+mas1.sort()
+for k in 0 ... mas1.count - 2{
+    if mas1[k] == mas1[k + 1]{
+        mas2.append(mas1[k])
+    }
+}
+mas1 = mas2
 
